a4c311a802da43cdf35d67b20774bae23bc8a7fd
Ragavi V G <ragavi_g@blr.trigent.com>
2026-07-09 00:57:37 +0530

    updated the api urls

--- src/app/shared/config/api-endpoints.ts
+++ src/app/shared/config/api-endpoints.ts
@@ -1,10 +1,10 @@
-const BASE = 'http://localhost:3005';
+// const BASE = 'http://localhost:3005';
 
 export const API_ENDPOINTS = {
-  submissionListAccountListUrl: `${BASE}/master_policies`,
-  submissionListAccountListActivityUrl: `http://localhost:3001/policy_tasks`,
-  glRatingTabLocationUrl: `http://localhost:3006/policy_general_liability`,
-  glRatingTabPolicyInputsUrl: `http://localhost:3002/gl/master_policies`,
-  glRatingTabUnderwritingReviewUrl: `http://localhost:3004/gl/under_writing`,
-  glRatingTabRatingUrl: `http://localhost:3003/gl/rating_screen`,
+  submissionListAccountListUrl: `https://avalonvmasterpolicies-bmcnaag0fac5ejeg.eastasia-01.azurewebsites.net/master_policies`,
+  submissionListAccountListActivityUrl: `https://avalonbackend-gsbqd8gwbebdanhw.eastasia-01.azurewebsites.net/policy_tasks`,
+  glRatingTabLocationUrl: `https://avalonvpolicygeneralliability-beadh9bdcffacnfh.eastasia-01.azurewebsites.net/policy_general_liability`,
+  glRatingTabPolicyInputsUrl: `https://avalonbackend1-guhkbqb5f0ereheq.eastasia-01.azurewebsites.net/gl/master_policies`,
+  glRatingTabUnderwritingReviewUrl: `https://avalonvglunderwriting-d2ggcyfyedd6fcd3.eastasia-01.azurewebsites.net/gl/under_writing`,
+  glRatingTabRatingUrl: `https://avalonvglratingscreen-hvh2aadmhme5h2d7.eastasia-01.azurewebsites.net/gl/rating_screen`,
 };
